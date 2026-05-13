97c941609d870bd6dd639f6d866407652701f8b6
Denise Cabrera <cabrera.denisep@gmail.com>
2026-05-12 23:32:07 -1000

    hero done

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -4,17 +4,15 @@ import './hero.css'
 function Hero() {
     return (
         <section className="hero">
-            <p className="greeting">Hello! My name is</p>
             <h1 className="hero-title">Denise Pawleen Cabrera</h1>
-            <p className="hero-description">Computer Science Student @ Yale University</p>
-            <div className="item-line">
-                <p className="item-name">SWE Intern</p>
-                <p className="item-name">&&</p>
-                <p className="item-name">Undergraduate Learning Assistant (CS - Data Structures)</p>
-            </div>
-            <div className="links-line">
-                <a href="https://github.com/denise-cbrr" className="link">GitHub</a>
-                <a href="https://www.linkedin.com/in/denisepawleencabrera/" className="link">LinkedIn</a>
+            <div className="hero-content">
+                <p className="primary">Third Year Computer Science Student @ Yale University</p>
+                <p className="secondary">SWE Intern & Undergraduate Learning Assistant (Data Structures)</p>
+                
+                <div className="links-line">
+                    <a href="https://github.com/denise-cbrr" className="link">GitHub</a>
+                    <a href="https://www.linkedin.com/in/denisepawleencabrera/" className="link">LinkedIn</a>
+                </div>
             </div>
 
             <div className="scroll-indicator">
