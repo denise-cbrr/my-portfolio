13113cc127b26daa8a4fd136a98aeaef8e7c6e27
Denise Cabrera <cabrera.denisep@gmail.com>
2026-05-13 00:07:53 -1000

    about section created

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import Hero from './components/hero/hero'
+import About from './components/about/about'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
   return (
     <main>
       <Hero />
+      <About />
     </main>
   )
 }
---
+++ src/components/about/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './about.css'
+
+function About() {
+    return (
+        <section className="about">
+           <h2>How I Think</h2>
+        </section>
+    )
+}
+
+export default About
